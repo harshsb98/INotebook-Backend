f3a73781beb577390a165bf3f089871380531b97
Harsh Sharma <harshsb98@gmail.com>
2022-03-15 12:30:59 +0530

    Storing data

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const User= require('../models/User')
 
-router.get('/',(req,res)=>{
-    obj={
-        a: 'thios',
-        number: 34
-    }
-    res.json(obj)
+
+router.post('/',(req,res)=>{
+console.log(req.body);
+const user= User(req.body);
+user.save();
+res.send(req.body);
 })
 
 module.exports= router
